3a983da176fc915cb7549486dc2e845822ca9a5a
natali-aharoni <natali.aharoni.computer.science@gmail.com>
2025-10-29 22:37:17 +0200

    implement pkg/logger/logger.go

---
+++ pkg/logger/logger.go
@@ -0,0 +1,72 @@
+package logger
+
+import (
+	"io"
+	"log/slog"
+	"os"
+)
+
+const (
+	EMPTY = ""
+	DEBUG = "debug"
+	INFO  = "info"
+	WARN  = "warn"
+	ERROR = "error"
+	JSON  = "json"
+)
+
+type Logger struct {
+	*slog.Logger
+}
+
+type Config struct {
+	Level     string
+	Format    string
+	Output    io.Writer
+	AddSource bool
+	Service   string
+}
+
+func New(cfg Config) *Logger {
+	if cfg.Output == nil {
+		cfg.Output = os.Stdout
+	}
+	if cfg.Format == EMPTY {
+		cfg.Format = JSON
+	}
+	if cfg.Level == EMPTY {
+		cfg.Level = INFO
+	}
+	var level slog.Level
+	switch cfg.Level {
+	case DEBUG:
+		level = slog.LevelDebug
+	case INFO:
+		level = slog.LevelInfo
+	case WARN:
+		level = slog.LevelWarn
+	case ERROR:
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	var handler slog.Handler
+	opts := &slog.HandlerOptions{
+		Level:     level,
+		AddSource: cfg.AddSource,
+	}
+	if cfg.Format == JSON {
+		handler = slog.NewJSONHandler(cfg.Output, opts)
+	} else {
+		handler = slog.NewTextHandler(cfg.Output, opts)
+	}
+
+	if cfg.Service != EMPTY {
+		handler = handler.WithAttrs([]slog.Attr{
+			slog.String("service", cfg.Service),
+		})
+	}
+
+	return &Logger{Logger: slog.New(handler)}
+}
