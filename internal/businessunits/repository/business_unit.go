df06044673edfb475b77a4d7d597609f306d33e6
natali-aharoni <natali.aharoni.computer.science@gmail.com>
2025-10-30 15:45:32 +0200

    remove comments

--- internal/businessunits/repository/business_unit.go
+++ internal/businessunits/repository/business_unit.go
@@ -1,12 +1,5 @@
 package repository
 
-// TODO: Implement data access layer for Business Units
-// Responsibilities:
-// - MongoDB CRUD operations
-// - Query methods (FindByID, FindAll, Search, etc.)
-// - Use models from pkg/model/business_unit.go
-// - No business logic here - pure data access
-
 import (
 	"context"
 	"errors"
